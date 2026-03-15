e692fb591d5656288badd74275bdb498e3c8531a
guccis <guccis1337@gmail.com>
2026-03-16 00:48:28 +0300

    Add files via upload

---
+++ balbal.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9be028df6511e5b121e5c2dfef420e02.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/guccis1337-cloud/quick-event/refs/heads/main/quick-time-event.lua"))()
