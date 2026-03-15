817c6a8a238c7ea5c162d11f21e3348452390cf0
guccis <guccis1337@gmail.com>
2026-03-16 00:52:58 +0300

    Update balbal.lua

--- balbal.lua
+++ balbal.lua
@@ -1,5 +1,9 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9be028df6511e5b121e5c2dfef420e02.lua"))()
+pcall(function()
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9be028df6511e5b121e5c2dfef420e02.lua"))()
+end)
 
-task.wait(1) -- 1 saniye bekleme payı bırakıyoruz
+task.wait(2)
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/guccis1337-cloud/quick-event/refs/heads/main/quick-time-event.lua"))()
+pcall(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/guccis1337-cloud/quick-event/refs/heads/main/quick-time-event.lua"))()
+end)
