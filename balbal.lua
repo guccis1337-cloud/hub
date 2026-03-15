86e5b5fbdbaf36f6c2f2c6b36aed82f6d0358a59
guccis <guccis1337@gmail.com>
2026-03-16 00:51:30 +0300

    Update balbal.lua

--- balbal.lua
+++ balbal.lua
@@ -1,3 +1,5 @@
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/9be028df6511e5b121e5c2dfef420e02.lua"))()
 
+task.wait(1) -- 1 saniye bekleme payı bırakıyoruz
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/guccis1337-cloud/quick-event/refs/heads/main/quick-time-event.lua"))()
